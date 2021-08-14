ace9a0105100dcf9feb1ac8759f26e8ed1ba562e
Joao Pedro <joaopedroprysthon@gmail.com>
2021-08-14 18:11:23 -0300

    inicio do projeto

---
+++ src/test/java/br/com/pedro/listadejogos/ListadejogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.listadejogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListadejogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
