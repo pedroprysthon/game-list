04c2cc60be227e5276b8bb21a02adf48e760b405
Joao Pedro <joaopedroprysthon@gmail.com>
2021-08-20 00:01:20 -0300

    feat: implemented crud methods at controller

--- src/main/java/br/com/pedro/listadejogos/ListadejogosApplication.java
+++ src/main/java/br/com/pedro/listadejogos/ListadejogosApplication.java
@@ -1,50 +1,14 @@
 package br.com.pedro.listadejogos;
 
-import br.com.pedro.listadejogos.models.Games;
-import br.com.pedro.listadejogos.models.GamesCategory;
-import br.com.pedro.listadejogos.repositories.GamesCategoryRepository;
-import br.com.pedro.listadejogos.repositories.GamesRepository;
-import br.com.pedro.listadejogos.repositories.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
-public class ListadejogosApplication implements CommandLineRunner {
+public class ListadejogosApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListadejogosApplication.class, args);
 	}
 
-	@Autowired
-	private GamesCategoryRepository gamesCategoryRepository;
-
-	@Autowired
-	private GamesRepository gamesRepository;
-
-	@Autowired
-	private UsersRepository usersRepository;
-
-	@Override
-	public void run(String... args) throws Exception {
-		Optional<GamesCategory> category = this.gamesCategoryRepository.findById(1l);
-		Games games = new Games();
-
-		if (category.isPresent()) {
-			games.setGamesCategory(category.get());
-		}
-
-		games.setTitle("Rocket League");
-		games.setDescription("jogo de carro que bate bola");
-		games.setStartingDate(new Date(System.currentTimeMillis()));
-		games.setReviewScore(5);
-		games.setReviewNote("good game");
-		games.setImageUrl("https://www.oficinadanet.com.br/imagens/post/32824/egs-social-rocketleague-news-1920x1080-1920x1080-975383433.jpg");
-		this.gamesRepository.save(games);
-	}
 }
--- src/main/java/br/com/pedro/listadejogos/controllers/GamesController.java
+++ src/main/java/br/com/pedro/listadejogos/controllers/GamesController.java
@@ -5,22 +5,71 @@ import br.com.pedro.listadejogos.models.Games;
 import br.com.pedro.listadejogos.repositories.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/games")
+@RequestMapping(value="/games")
+@CrossOrigin(origins = "http://localhost:3000")
 public class GamesController {
     @Autowired
     private GamesRepository gamesRepository;
 
     @GetMapping
-    public ResponseEntity<List<Games>> getGames(){
+    public ResponseEntity<List<Games>> findAllGames(){
         List<Games> games = gamesRepository.findAll();
 
+        //return ResponseEntity.status(HttpStatus.OK).body(games);
         return ResponseEntity.ok(games);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Games> findGameById(@PathVariable("id") Long id) {
+        Optional<Games> games = gamesRepository.findById(id);
+
+        if (games.isPresent()) {
+            return ResponseEntity.ok(games.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Games> saveGame(@RequestBody Games newGame) {
+
+        Games created = this.gamesRepository.save(newGame);
+
+        return ResponseEntity.ok(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Games> updateGame(@PathVariable("id") Long id, @RequestBody Games newGame) {
+        Optional<Games> games = gamesRepository.findById(id);
+
+        if (games.isPresent()) {
+            //Games oldGame = games.get();//
+            //oldGame.setTitle(newGame.getTitle());
+            //oldGame.setDescription(newGame.getDescription());
+            //oldGame = this.gamesRepository.save(oldGame);
+            newGame.setId(id);
+            this.gamesRepository.save(newGame);
+            return ResponseEntity.ok(newGame);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteGame(@PathVariable("id") Long id) {
+        Optional<Games> games = gamesRepository.findById(id);
+
+        if (games.isPresent()) {
+            this.gamesRepository.delete(games.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
